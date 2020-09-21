a03d4a811dc7b9f3f5d7b33fba7f11ce55e2bcae
Harshil Panchal <harshil@Harshils-MacBook-Pro.local>
2020-09-20 23:23:11 -0700

    changed related name post_list to posts;

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    description = models.TextField("Profile Description", max_length=1000, null=True)
+    description = models.TextField("Profile Description", max_length=1000, default='Default User Profile Description.')
     picture = models.ImageField(default='profile_pics/default.jpg', upload_to='profile_pics')
 
     def __str__(self):
